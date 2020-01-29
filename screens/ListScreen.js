472730b0765da8fe201013a0c5cb72bd654aa6a2
Lory LETICEE <loryleticee@gmail.com>
2020-01-29 15:12:29 +0100

    cache not use to show

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -3,24 +3,47 @@ import {
   StyleSheet,
   Text,
   FlatList,
+  AsyncStorage
 } from 'react-native';
 
 import getVelibs from "../service/velib"
 
 export default function ListScreen() {
-    const [velibs, setVelibs] = useState([]);
-    useEffect(() => {
-      getVelibs().then(data => {
-        setVelibs(data.records);
-      });
-    }, [])
+  const [velibs, setVelibs] = useState([]);
 
-  console.log(velibs);
+  _storeData = async (data) => {
+    try {
+      await AsyncStorage.setItem("velibs",JSON.stringify(data));
+    } catch (error) {
+      // Error saving data
+    }
+  };
+
+  _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('velib');
+      if (value !== null) {
+        // We have data!!
+        console.log('RetrieveData', value);
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+  };
+
+  useEffect(() => {
+    getVelibs().then(data => {
+      setVelibs(data.records);
+      _storeData(data.records);
+
+    });
+  }, [])
 
     return (
     <FlatList
       style={styles.container}
       renderItem={({ item }) => {
+
         return <Text>{item.fields.station_name}</Text>
       }}
       data={velibs}
